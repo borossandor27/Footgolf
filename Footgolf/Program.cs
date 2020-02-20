90329a6d6a4537614830d6ee48fbad2462753443
borossandor27 <boros.sanyi@gmail.com>
2020-02-20 12:05:37 +0100

    Add project files.

---
+++ Footgolf/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Footgolf
+{
+    class Program
+    {
+        static List<Versenyzo> versenyzok = new List<Versenyzo>();
+        static void Main(string[] args)
+        {
+            Beolvas(@"..\..\fob2016.txt");
+            Console.WriteLine($"\n3. feladat: Versenyzők száma: {versenyzok.Count}");
+            
+            double atl = 100.0 * versenyzok.Count(a => a.Kategoria.Equals("Noi")) / versenyzok.Count;
+            Console.WriteLine($"\n4. feladat: A női versenyzők aránya: {atl.ToString("0.00")}%");
+
+            Console.WriteLine("\n6. feladat: A bajnok női versenyző");
+            Versenyzo noiBajnok = versenyzok.Find(a => a.Kategoria.Equals("Noi") && a.Osszpont == versenyzok.FindAll(b => b.Kategoria.Equals("Noi")).Max(c => c.Osszpont));
+            Console.WriteLine($"\tNév: {noiBajnok.Nev}");
+            Console.WriteLine($"\tEgyesület: {noiBajnok.Egyesulet}");
+            Console.WriteLine($"\tÖsszpont: {noiBajnok.Osszpont}");
+
+            Console.WriteLine("\n7. feladat: ");
+            Kiir();
+
+            Console.WriteLine("\n8. feladat: Egyesület statisztika");
+
+            foreach (var item in versenyzok.GroupBy(a => a.Egyesulet).Select(b => new { egyesulet = b.Key, fo = b.Count() }).Where(c => c.fo > 2 && !c.egyesulet.Equals("n.a."))) 
+            {
+                Console.WriteLine($"\t{item.egyesulet} - {item.fo} fő");
+            }
+            Console.WriteLine("\nProgram vége!");
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// 2. Olvassa be a fob2016.txt állományban lévő adatokat és tárolja el
+        ///    egy olyan adatszerkezetben, amely a további feladatok megoldására 
+        ///    alkalmas!
+        /// </summary>
+        /// <param name="forras">fob2016.txt elérési úttal</param>
+        static void Beolvas(string forras)
+        {
+            Console.WriteLine("2. feladat: Adatok beolvasása...");
+            try
+            {
+                using (StreamReader sr = new StreamReader(forras))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        versenyzok.Add(new Versenyzo(sr.ReadLine().Split(';')));
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+        }
+
+        static void Kiir()
+        {
+            Console.WriteLine("Férfi versenyzők kiírása...");
+            using (StreamWriter sw = new StreamWriter("osszpontFF.txt"))
+            {
+                foreach (var item in versenyzok.FindAll(a => a.Kategoria.Equals("Felnott ferfi")))
+                {
+                    sw.WriteLine($"{item.Nev};{item.Osszpont}");
+                }
+            }
+        }
+    }
+}
---
+++ Footgolf/Versenyzo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footgolf
+{
+    class Versenyzo
+    {
+        static readonly int fordulok = 8;
+        string nev;
+        string kategoria;
+        string egyesulet;
+        int[] pontok = new int[fordulok];
+        int osszpont;
+
+        public string Nev { get => nev; set => nev = value; }
+        public string Kategoria { get => kategoria; set => kategoria = value; }
+        public string Egyesulet { get => egyesulet; set => egyesulet = value; }
+        public int[] Pontok { get => pontok; set => pontok = value; }
+        public int Osszpont { get => osszpont; set => osszpont = value; }
+
+        public Versenyzo(string[] sor)
+        {
+            Nev = sor[0];
+            Kategoria = sor[1];
+            Egyesulet = sor[2];
+            for (int i = 0; i < fordulok; i++)
+            {
+                pontok[i] = int.Parse(sor[i + 3]);
+            }
+            osszpont = osszpontszam();
+        }
+
+        int osszpontszam()
+        {
+            int ossz = 0;
+            int[] p = pontok.OrderByDescending(a => a).ToArray(); //-- rendezett másolat készítés
+            for (int i = 0; i < 6; i++)
+            {
+                ossz += p[i];
+            }
+            /*
+             * Ha a versenyző legrosszabb egy vagy két eredménye nem nulla,
+             * akkor a versenyzőnek az összpontszámába bele kell számítani
+             * azt a 10 pont bónuszt, amelyet ezekben a fordulókban megkapott.
+             */
+            ossz += p[6] > 0 ? 10 : 0;
+            ossz += p[7] > 0 ? 10 : 0;
+            return ossz;
+        }
+
+    }
+}
